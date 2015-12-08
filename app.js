8a87c811dd39d17a7a70c0572fde601d6b0ce594
Alex Donley <happylumberjack@gmail.com>
2015-12-08 13:15:20 -0500

    Had Initial login system connected, there seems to be a problem in the promise system when it attempts to fetch the data using passport from the db through bookshelf. -Alex

--- app.js
+++ app.js
@@ -1,60 +1,86 @@
+// vendor libraries
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bcrypt = require('bcrypt-nodejs');
+var ejs = require('ejs');
+var path = require('path');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// custom libraries
+// routes
+var route = require('./route.js');
+// model
+var Model = require('./models/user.js');
 
 var app = express();
 
-// view engine setup
+passport.use(new LocalStrategy(function(username, password, done) {
+  new Model.User({username: username}).fetch().then(function(data) {
+    var user = data;
+    if(user === null) {
+      return done(null, false, {message: 'Invalid username or password'});
+    } else {
+      user = data.toJSON();
+      if(!bcrypt.compareSync(password, user.password)) {
+        return done(null, false, {message: 'Invalid username or password'});
+      } else {
+        return done(null, user);
+      }
+    }
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+  new Model.User({username: username}).fetch().then(function(user) {
+    done(null, user);
+  });
+});
+
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser());
+app.use(session({secret: 'secret strategic xxzzz code'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', routes);
-app.use('/users', users);
+// GET
+app.get('/', route.index);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// signin
+// GET
+app.get('/signin', route.signIn);
+// POST
+app.post('/signin', route.signInPost);
+
+// signup
+// GET
+app.get('/signup', route.signUp);
+// POST
+app.post('/signup', route.signUpPost);
+
+// logout
+// GET
+app.get('/signout', route.signOut);
+
+/********************************/
+
+/********************************/
+// 404 not found
+app.use(route.notFound404);
+
+var server = app.listen(app.get('port'), function(err) {
+  if(err) throw err;
+
+  var message = 'Server is running @ http://localhost:' + server.address().port;
+  console.log(message);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
