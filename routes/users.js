5e6b0f679c89a00266b903b42ddf4aa54a8dbbc2
Alex Donley <happylumberjack@gmail.com>
2015-12-08 14:49:52 -0500

    Working Account Creation. Merge with extra functionality from previous version. Move HTML to views and have it appear using ejs. -Alex

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,59 @@
-var express = require('express');
-var router = express.Router();
+var Model = require('./../models/User');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* Save a user */
+var saveUser = function (req, res) {
+	new Model.User({
+		EMAIL: req.body.email,
+		FIRST_NAME: req.body.first_name,
+		LAST_NAME: req.body.last_name,
+	}).save()
+		.then(function (user) {
+			res.json(user);
+		}).catch(function (error) {
+			console.log(error);
+			res.send('An error occured');
+		});
+};
 
-module.exports = router;
+/* Get all users */
+var getAllUsers = function (req, res) {
+	new Model.User().fetchAll()
+		.then(function (users) {
+			res.json(users);
+		}).catch(function (error) {
+			console.log(error);
+			res.send('An error occured');
+		});
+};
+
+/* Delete a user */
+var deleteUser = function (req, res) {
+	var userId = req.params.id;
+	new Model.User().where('id', userId)
+		.destroy()
+		.catch(function (error) {
+			console.log(error);
+			res.send('An error occured');
+		});
+};
+
+/* Get a user */
+var getUser = function (req, res) {
+	var userId = req.params.id;
+	new Model.User().where('id', userId)
+		.fetch()
+		.then(function (user) {
+			res.json(user);
+		}).catch(function (error) {
+			console.log(error);
+			res.send('An error occured');
+		});
+};
+
+/* Exports all methods */
+module.exports = {
+	saveUser: saveUser,
+	getAllUsers: getAllUsers,
+	deleteUser: deleteUser,
+	getUser: getUser
+};
