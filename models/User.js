ea725c301259cd0f2debe5e90f48b9ee8d8fd12d
Alex Donley <happylumberjack@gmail.com>
2015-12-11 16:14:58 -0500

    Can create new user account need to be able to fetch both the user info and password info.

--- models/User.js
+++ models/User.js
@@ -6,8 +6,20 @@ var DB = require('../config/db').DB;
 var User = DB.Model.extend({
     tableName: 'USER_ACCOUNT',
     idAttribute: 'USER_ACCOUNT_ID',
+    password: function() {
+        return this.hasOne(Password);
+    }
 });
 
+var Password = DB.Model.extend({
+    tableName: 'PASSWORD',
+    idAttribute: 'PASSWORD_ID',
+    user: function() {
+        return this.belongsTo(User);
+    }
+})
+
 module.exports = {
-    User: User
+    User: User,
+    Password: Password
 };
--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,35 @@ var saveUser = function (req, res) {
 	new Model.User({
 		EMAIL_ADDRESS: req.body.email,
 		FIRST_NAME: req.body.first_name,
-		LAST_NAME: req.body.last_name,
+		LAST_NAME: req.body.last_name
+	}).save();
+
+	//Todo save password directly into User through FK
+	new Model.Password({
+		PASSWORD: req.body.password,
+		USER_ACCOUNT_ID: req.params.id
 	}).save()
-		.then(function (user) {
-			res.json(user);
-		}).catch(function (error) {
-			console.log(error);
-			res.send('An error occured');
-		});
+			.then(function (user) {
+				res.json(user);
+			}).catch(function (error) {
+		console.log(error);
+		res.send('An error occured');
+	});
+
 };
 
 /* Get all users */
 var getAllUsers = function (req, res) {
 	new Model.User().fetchAll()
-		.then(function (users) {
-			res.json(users);
-		}).catch(function (error) {
-			console.log(error);
-			res.send('An error occured');
-		});
+        .then(function (users) {
+            res.json(users);
+        }).catch(function (error) {
+        console.log(error);
+        res.send('An error occured');
+    });
+//ToDo Fetch all the user information and the password associate with it.
+    //new Model.Password.where({USER_ACCOUNT_ID: req.params.id}).fetch({withRelated: ['USER_ACCOUNT']}).then(function(password) {
+    //  console.log(JSON.stringify(password.related('USER_ACCOUNT')));
 };
 
 /* Delete a user */
