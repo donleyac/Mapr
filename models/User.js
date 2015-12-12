6c9bb903ae066e0641a560a8026be626f70fec18
Alex Donley <happylumberjack@gmail.com>
2015-12-11 21:53:10 -0500

    Add relation between the password and the user. Some of the sql that is being autogenerated is causing some problems.
    -Alex

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ var DB = require('../config/db').DB;
 var User = DB.Model.extend({
     tableName: 'USER_ACCOUNT',
     idAttribute: 'USER_ACCOUNT_ID',
-    password: function() {
+    passwords: function() {
         return this.hasOne(Password);
     }
 });
@@ -14,12 +14,12 @@ var User = DB.Model.extend({
 var Password = DB.Model.extend({
     tableName: 'PASSWORD',
     idAttribute: 'PASSWORD_ID',
-    user: function() {
+    users: function() {
         return this.belongsTo(User);
     }
 })
 
 module.exports = {
-    User: User,
-    Password: Password
+    Users: User,
+    Passwords: Password
 };
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,14 @@ var Model = require('./../models/User');
 
 /* Save a user */
 var saveUser = function (req, res) {
-	new Model.User({
+	new Model.Users({
 		EMAIL_ADDRESS: req.body.email,
 		FIRST_NAME: req.body.first_name,
 		LAST_NAME: req.body.last_name
 	}).save();
 
 	//Todo save password directly into User through FK
-	new Model.Password({
+	new Model.Passwords({
 		PASSWORD: req.body.password,
 		USER_ACCOUNT_ID: req.params.id
 	}).save()
@@ -24,22 +24,23 @@ var saveUser = function (req, res) {
 
 /* Get all users */
 var getAllUsers = function (req, res) {
-	new Model.User().fetchAll()
-        .then(function (users) {
-            res.json(users);
-        }).catch(function (error) {
-        console.log(error);
-        res.send('An error occured');
-    });
+	new Model.Passwords().fetchAll({withRelated: ['users'], debug:true})
+			.then(function (users) {
+				console.log(JSON.stringify(users));
+				res.json(users);
+			}).catch(function (error) {
+		console.log(error);
+		res.send('An error occured');
 //ToDo Fetch all the user information and the password associate with it.
-    //new Model.Password.where({USER_ACCOUNT_ID: req.params.id}).fetch({withRelated: ['USER_ACCOUNT']}).then(function(password) {
-    //  console.log(JSON.stringify(password.related('USER_ACCOUNT')));
-};
+		//new Model.Password.where({USER_ACCOUNT_ID: req.params.id}).fetch({withRelated: ['USER_ACCOUNT']}).then(function(password) {
+		//  console.log(JSON.stringify(password.related('USER_ACCOUNT')));
+	});
+}
 
 /* Delete a user */
 var deleteUser = function (req, res) {
 	var userId = req.params.id;
-	new Model.User().where('id', userId)
+	new Model.Users().where('id', userId)
 		.destroy()
 		.catch(function (error) {
 			console.log(error);
@@ -50,7 +51,7 @@ var deleteUser = function (req, res) {
 /* Get a user */
 var getUser = function (req, res) {
 	var userId = req.params.id;
-	new Model.User().where('id', userId)
+	new Model.UserS().where('id', userId)
 		.fetch()
 		.then(function (user) {
 			res.json(user);
