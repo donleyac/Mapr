66efb10b5e70a893d811a645152c2de5659d0fb7
Alex Donley <happylumberjack@gmail.com>
2015-12-12 15:45:54 -0500

    Sign up and display all users is working. Password and user models are combined now.

--- models/User.js
+++ models/User.js
@@ -5,21 +5,9 @@ var DB = require('../config/db').DB;
 
 var User = DB.Model.extend({
     tableName: 'USER_ACCOUNT',
-    idAttribute: 'USER_ACCOUNT_ID',
-    passwords: function() {
-        return this.hasOne(Password);
-    }
+    idAttribute: 'USER_ACCOUNT_ID'
 });
 
-var Password = DB.Model.extend({
-    tableName: 'PASSWORD',
-    idAttribute: 'PASSWORD_ID',
-    users: function() {
-        return this.belongsTo(User);
-    }
-})
-
 module.exports = {
-    Users: User,
-    Passwords: Password
+    Users: User
 };
--- routes/users.js
+++ routes/users.js
@@ -1,41 +1,49 @@
 var Model = require('./../models/User');
-
+var bcrypt = require('bcrypt-nodejs');
 /* Save a user */
-var saveUser = function (req, res) {
-	new Model.Users({
-		EMAIL_ADDRESS: req.body.email,
-		FIRST_NAME: req.body.first_name,
-		LAST_NAME: req.body.last_name
-	}).save();
+// POST
+var signUpPost = function(req, res, next) {
+    var user = req.body;
+    var usernamePromise = null;
+    usernamePromise = new Model.Users({EMAIL_ADDRESS: user.email}).fetch();
 
-	//Todo save password directly into User through FK
-	new Model.Passwords({
-		PASSWORD: req.body.password,
-		USER_ACCOUNT_ID: req.params.id
-	}).save()
-			.then(function (user) {
-				res.json(user);
-			}).catch(function (error) {
-		console.log(error);
-		res.send('An error occured');
-	});
+    return usernamePromise.then(function(model) {
+        if(model) {
+            res.render('signup', {title: 'signup', errorMessage: 'username already exists'});
+        } else {
+            //****************************************************//
+            // MORE VALIDATION GOES HERE(E.G. PASSWORD VALIDATION)
+            //****************************************************//
+            var password = user.password;
+            var hash = bcrypt.hashSync(password);
 
+            var signUpUser = new Model.Users({
+                EMAIL_ADDRESS: user.email,
+                FIRST_NAME: user.first_name,
+                LAST_NAME: user.last_name,
+                PASSWORD: hash
+            });
+
+            signUpUser.save().then(function(model) {
+                // sign in the newly registered user
+                //signInPost(req, res, next);
+                res.json(model);
+            });
+        }
+    })
 };
 
 /* Get all users */
 var getAllUsers = function (req, res) {
-	new Model.Passwords().fetchAll({withRelated: ['users'], debug:true})
+	new Model.Users().fetchAll()
 			.then(function (users) {
 				console.log(JSON.stringify(users));
 				res.json(users);
 			}).catch(function (error) {
-		console.log(error);
-		res.send('An error occured');
-//ToDo Fetch all the user information and the password associate with it.
-		//new Model.Password.where({USER_ACCOUNT_ID: req.params.id}).fetch({withRelated: ['USER_ACCOUNT']}).then(function(password) {
-		//  console.log(JSON.stringify(password.related('USER_ACCOUNT')));
-	});
-}
+        console.log(error);
+        res.send('An error occured');
+    })
+};
 
 /* Delete a user */
 var deleteUser = function (req, res) {
@@ -63,8 +71,9 @@ var getUser = function (req, res) {
 
 /* Exports all methods */
 module.exports = {
-	saveUser: saveUser,
-	getAllUsers: getAllUsers,
-	deleteUser: deleteUser,
-	getUser: getUser
+    saveUser: signUpPost,
+    getAllUsers: getAllUsers,
+    deleteUser: deleteUser,
+    getUser: getUser
 };
+
